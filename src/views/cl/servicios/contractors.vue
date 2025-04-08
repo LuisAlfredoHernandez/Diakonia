9d8e90993c6bcaba1c72b0ab62ba5f25a8c1cec5
Luis Alfredo Hernandez <lhernandez@flexus.local>
2025-04-08 16:01:57 -0400

    creating card component

--- src/views/cl/servicios/contractors.vue
+++ src/views/cl/servicios/contractors.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="grid grid-cols-1 lg:grid-cols-1 place-items-start legacy">
     <div class="grid grid-cols-1 lg:grid-cols-1 place-items-start">
-      <pre>Distancia en radio (km): {{ rangeValue }}</pre>
+      <pre>Distancia en radio (km) a tu posicion: {{ rangeValue }}</pre>
       <Range v-model="rangeValue" :max="'15'" :min="'0'" label="" />
     </div>
 
     <div class="mt-5 grid grid-cols-1 lg:grid-cols-1 place-items-start">
-      <pre>Localidad distinta a la suya?</pre>
+      <pre class="mb-1">Localidad distinta a la suya?</pre>
       <Select
         v-model="selectedCountryCode"
         :options="countryOptions"
@@ -20,45 +20,95 @@
         v-model="selectedCityCode"
         :options="cityOptions"
         :disabled="!selectedCountryCode"
-        placeholder="Selecciona una ciudad"
-        class="w-full mb-4"
+        placeholder="Selecciona un sector"
+        class="w-full mt-1"
       />
     </div>
 
-    <div class="mt-20">
-      <TheCard
-        img-alt="Desk"
-        img-src="https://flowbite.com/docs/images/blog/image-4.jpg"
-        variant="horizontal"
-      >
-        <div class="p-5">
-          <div>
-            <label class="mt-2 mr-2" for="Name">Servicios realizados: 22</label>
-            <Button>
+    <div class="mt-10">
+      <TheCard variant="horizontal">
+        <div class="grid grid-cols-2">
+          <div class="col1Container">
+            <p class="text-sm">Servicios realizados: 22</p>
+            <NavbarLink class="mr-2 list-none italic mt-5"
+              >Ver Comentarios
+            </NavbarLink>
+            <p class="mt-10">Ult vez hace (hr/min): {{ "02:15" }}</p>
+            <Button class="mt-5">
               <template #prefix>
                 <svg
-                  class="mt-5 w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
+                  fill="#ffffff"
+                  height="20px"
+                  width="20px"
+                  version="1.1"
+                  id="Layer_1"
                   xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 458 458"
+                  xml:space="preserve"
+                  stroke="#ffffff"
                 >
-                  <path
-                    d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
-                  />
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <g>
+                      <g>
+                        <path
+                          d="M428,41.534H30c-16.569,0-30,13.431-30,30v252c0,16.568,13.432,30,30,30h132.1l43.942,52.243 c5.7,6.777,14.103,10.69,22.959,10.69c8.856,0,17.258-3.912,22.959-10.69l43.942-52.243H428c16.568,0,30-13.432,30-30v-252 C458,54.965,444.568,41.534,428,41.534z M323.916,281.534H82.854c-8.284,0-15-6.716-15-15s6.716-15,15-15h241.062 c8.284,0,15,6.716,15,15S332.2,281.534,323.916,281.534z M67.854,198.755c0-8.284,6.716-15,15-15h185.103c8.284,0,15,6.716,15,15 s-6.716,15-15,15H82.854C74.57,213.755,67.854,207.039,67.854,198.755z M375.146,145.974H82.854c-8.284,0-15-6.716-15-15 s6.716-15,15-15h292.291c8.284,0,15,6.716,15,15C390.146,139.258,383.43,145.974,375.146,145.974z"
+                        ></path>
+                      </g>
+                    </g>
+                  </g>
                 </svg>
               </template>
               Contactar
             </Button>
           </div>
-          <h5
-            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-          >
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
+          <div class="verificationContainer">
+            <div class="flex flex-cols-2 justify-center">
+              <p class="text-sm underline italic text-center mt-5">
+                {{ isProviderVerified ? "Verificado" : "Sin verficicar" }}
+              </p>
+              <svg
+                class="mt-5"
+                fill="#0033ff"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                width="24px"
+                height="24px"
+                viewBox="-100.65 -53.65 643.84 643.84"
+                xml:space="preserve"
+                stroke="#0033ff"
+                stroke-width="15.023148000000003"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <g>
+                    <g>
+                      <path
+                        d="M496.785,152.779c-3.305-25.085-16.549-51.934-38.826-74.205c-22.264-22.265-49.107-35.508-74.186-38.813 c-11.348-1.499-26.5-7.766-35.582-14.737C328.111,9.626,299.764,0,268.27,0s-59.841,9.626-79.921,25.024 c-9.082,6.965-24.235,13.238-35.582,14.737c-25.08,3.305-51.922,16.549-74.187,38.813c-22.277,22.271-35.521,49.119-38.825,74.205 c-1.493,11.347-7.766,26.494-14.731,35.57C9.621,208.422,0,236.776,0,268.27s9.621,59.847,25.024,79.921 c6.971,9.082,13.238,24.223,14.731,35.568c3.305,25.086,16.548,51.936,38.825,74.205c22.265,22.266,49.107,35.51,74.187,38.814 c11.347,1.498,26.5,7.771,35.582,14.736c20.073,15.398,48.421,25.025,79.921,25.025s59.841-9.627,79.921-25.025 c9.082-6.965,24.234-13.238,35.582-14.736c25.078-3.305,51.922-16.549,74.186-38.814c22.277-22.27,35.521-49.119,38.826-74.205 c1.492-11.346,7.766-26.492,14.73-35.568c15.404-20.074,25.025-48.422,25.025-79.921c0-31.494-9.621-59.848-25.025-79.921 C504.545,179.273,498.277,164.126,496.785,152.779z M439.256,180.43L246.477,373.209l-30.845,30.846 c-8.519,8.52-22.326,8.52-30.845,0l-30.845-30.846l-56.665-56.658c-8.519-8.52-8.519-22.326,0-30.846l30.845-30.844 c8.519-8.519,22.326-8.519,30.845,0l41.237,41.236L377.561,118.74c8.52-8.519,22.326-8.519,30.846,0l30.844,30.845 C447.775,158.104,447.775,171.917,439.256,180.43z"
+                      ></path>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <img
+              src="https://img.freepik.com/fotos-premium/hombre-joven-aislado-cara-neutral_75922-474.jpg"
+              alt=""
+            />
+          </div>
         </div>
       </TheCard>
     </div>
@@ -68,49 +118,78 @@
 <script setup lang="ts">
 import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
-import { Button, Input, Select, Range, TheCard } from "flowbite-vue";
+import {
+  Button,
+  Input,
+  Select,
+  Range,
+  TheCard,
+  NavbarLink,
+} from "flowbite-vue";
 import { useEpiStore } from "../../../store/modules/platform";
 
-const rangeValue = ref("10");
+const rangeValue = ref("0");
+
+const isProviderVerified = ref(true);
 
 interface Landmark {
   code: string;
   name: string;
 }
 
-interface Country extends Landmark {
+interface Cities extends Landmark {
   cities: Landmark[];
 }
 
-const countries: Country[] = [
+const countries: Cities[] = [
   {
-    name: "España",
-    code: "ES",
+    code: "SDE",
+    name: "Santo Domingo Este",
     cities: [
       {
-        code: "madrid",
-        name: "Madrid",
+        code: "SDEAR",
+        name: "Alma Rosa",
       },
       {
-        code: "bcn",
-        name: "Barcelona",
+        code: "SDEVF",
+        name: "Villa Faro",
+      },
+      {
+        code: "SDELI",
+        name: "La Isabelita",
+      },
+      {
+        code: "SDESI",
+        name: "San Isidro",
       },
     ],
   },
   {
-    code: "mx",
-    name: "México",
+    code: "DN",
+    name: "Distrito Nacional",
     cities: [
       {
-        code: "cdmx",
-        name: "CDMX",
+        code: "DNDBC",
+        name: "Don Bosco",
+      },
+      {
+        code: "DNCH",
+        name: "Cuesta Hermosa",
+      },
+      {
+        code: "DNYC",
+        name: "La Yuca",
+      },
+      {
+        code: "DNQ",
+        name: "Quisqueya",
       },
     ],
   },
 ];
 
-const selectedCountryCode = ref<string>("es"); // Código inicial
-const selectedCityCode = ref<string>("madrid");
+const selectedCountryCode = ref<string>(""); // Código inicial
+const selectedCityCode = ref<string>("");
 
 // ===== Opciones para los selects (formato {value, label}) =====
 const countryOptions = computed(() =>
